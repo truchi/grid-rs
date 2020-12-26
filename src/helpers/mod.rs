1879dd3f5399d35408cd7907ac5e81a58dc6cf90
Romain TRUCHI <romain.truchi.06@gmail.com>
2020-12-26 15:59:42 +0100

    Moved iters in types

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -1,7 +1,5 @@
 mod coords;
-mod iters;
 mod range;
 
 pub use coords::*;
-pub use iters::*;
 pub(crate) use range::*;
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,5 +1,7 @@
 mod grid1d;
+mod iters;
 mod majors;
 
 pub use grid1d::*;
+pub use iters::*;
 pub use majors::*;
