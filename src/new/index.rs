97226064f59671fca1e1658d73f36266deaf0ccd
Romain TRUCHI <romain.truchi.06@gmail.com>
2021-01-27 10:26:49 +0100

    Moved indexes into file

--- src/new/index.rs
+++ src/new/index.rs
@@ -1,124 +1,28 @@
-use crate::{Coord, Flat, Major, Point, ToRange};
+use crate::*;
 use std::ops::{Range, RangeBounds};
 
-mod index0d {
-    use super::*;
-
-    pub fn index<M: Major>(point: Point, size: M) -> usize {
-        let point = M::from(point);
-        point.minor() * size.major() + point.major()
-    }
-
-    pub unsafe fn get<M: Major, I, T: AsRef<[I]>>(point: Point, grid: &Flat<M, I, T>) -> &I {
-        let index = index(point, grid.size());
-        grid.as_ref().get_unchecked(index)
-    }
-
-    pub unsafe fn get_mut<M: Major, I, T: AsMut<[I]>>(
-        point: Point,
-        grid: &mut Flat<M, I, T>,
-    ) -> &mut I {
-        let index = index(point, grid.size());
-        grid.as_mut().get_unchecked_mut(index)
-    }
+pub trait Index0D {
+    fn unchecked<S: Into<Size>>(self, size: S) -> Point;
+    fn checked<S: Into<Size>>(self, size: S) -> Option<Point>;
 }
-
-mod index1d {
-    use super::*;
-
-    pub fn index<M: Major>(
-        (i, Range { start, end }): (usize, Range<usize>),
-        size: M,
-    ) -> Range<usize> {
-        let point = M::new(start, i).into();
-        let start = index0d::index(point, size);
-        start..start + end
-    }
-
-    pub unsafe fn get<M: Major, I, T: AsRef<[I]>>(
-        i: (usize, Range<usize>),
-        grid: &Flat<M, I, T>,
-    ) -> &[I] {
-        let range = index(i, grid.size());
-        grid.as_ref().get_unchecked(range)
-    }
-
-    pub unsafe fn get_mut<M: Major, I, T: AsMut<[I]>>(
-        i: (usize, Range<usize>),
-        grid: &mut Flat<M, I, T>,
-    ) -> &mut [I] {
-        let range = index(i, grid.size());
-        grid.as_mut().get_unchecked_mut(range)
-    }
-}
-
-macro_rules! index {
-    ($(($mod:ident) $Trait:ident -> $Output:ty: $Base:ty, $Index:ty,)*) => { $(
-        pub trait $Trait: Sized {
-            fn unchecked<M: Major>(self, size: M) -> $Base;
-            fn checked<M: Major>(self, size: M) -> Option<$Base>;
-
-            fn index<M: Major>(self, size: M) -> Option<$Index> {
-                Some($mod::index(self.checked(size)?, size))
-            }
-
-            fn index_unchecked<M: Major>(self, size: M) -> $Index {
-                $mod::index(self.unchecked(size), size)
-            }
-
-            fn get<M: Major, I, T: AsRef<[I]>>(self, grid: &Flat<M, I, T>) -> Option<&$Output> {
-                let index = self.checked(grid.size())?;
-                // SAFETY: index is checked
-                Some(unsafe { $mod::get(index, grid) })
-            }
-
-            fn get_mut<M: Major, I, T: AsMut<[I]>>(
-                self,
-                grid: &mut Flat<M, I, T>,
-            ) -> Option<&mut $Output> {
-                let index = self.checked(grid.size())?;
-                // SAFETY: point is checked
-                Some(unsafe { $mod::get_mut(index, grid) })
-            }
-
-            unsafe fn get_unchecked<M: Major, I, T: AsRef<[I]>>(
-                self,
-                grid: &Flat<M, I, T>,
-            ) -> &$Output {
-                let index = self.unchecked(grid.size());
-                $mod::get(index, grid)
-            }
-
-            unsafe fn get_unchecked_mut<M: Major, I, T: AsMut<[I]>>(
-                self,
-                grid: &mut Flat<M, I, T>,
-            ) -> &mut $Output {
-                let index = self.unchecked(grid.size());
-                $mod::get_mut(index, grid)
-            }
-        }
-    )* };
-}
-
-index!(
-    (index0d) Index0D -> I: Point, usize,
-    (index1d) Index1D -> [I]: (usize, Range<usize>), Range<usize>,
-);
 
 impl Index0D for Point {
-    fn unchecked<M: Major>(self, _: M) -> Point {
+    fn unchecked<S: Into<Size>>(self, size: S) -> Point {
         self
     }
 
-    fn checked<M: Major>(self, size: M) -> Option<Point> {
-        let point = self.unchecked(size);
-
-        if point < Into::<Point>::into(size) {
-            Some(point)
+    fn checked<S: Into<Size>>(self, size: S) -> Option<Point> {
+        if self < size.into() {
+            Some(self)
         } else {
             None
         }
     }
+}
+
+pub trait Index1D: Sized {
+    fn unchecked<M: Major>(self, size: M) -> (usize, Range<usize>);
+    fn checked<M: Major>(self, size: M) -> Option<(usize, Range<usize>)>;
 }
 
 impl Index1D for usize {
@@ -148,5 +52,84 @@ impl<T: RangeBounds<usize>> Index1D for (usize, T) {
 
     fn unchecked<M: Major>(self, size: M) -> (usize, Range<usize>) {
         (self.0, ToRange::unchecked(self.1, size.major()))
+    }
+}
+
+/// Indexing into a rectangle inside a grid, with optional cropping.
+pub trait Index2D {
+    /// Returns the index **with** bounds checking.
+    fn checked(self, size: Size) -> Option<Point<Range<usize>>>;
+
+    /// Returns the index **without** bounds checking.
+    fn unchecked(self, size: Size) -> Point<Range<usize>>;
+}
+
+impl Index2D for () {
+    fn checked(self, size: Size) -> Option<Point<Range<usize>>> {
+        Some(self.unchecked(size))
+    }
+
+    fn unchecked(self, size: Size) -> Point<Range<usize>> {
+        Point {
+            x: 0..size.x,
+            y: 0..size.x,
+        }
+    }
+}
+
+impl<X: RangeBounds<usize>, Y: RangeBounds<usize>> Index2D for Point<X, Y> {
+    fn checked(self, size: Size) -> Option<Point<Range<usize>>> {
+        (self.x, self.y).checked(size)
+    }
+
+    fn unchecked(self, size: Size) -> Point<Range<usize>> {
+        (self.x, self.y).unchecked(size)
+    }
+}
+
+impl<X: RangeBounds<usize>, Y: RangeBounds<usize>> Index2D for (X, Y) {
+    fn checked(self, size: Size) -> Option<Point<Range<usize>>> {
+        Some(Point {
+            x: ToRange::checked(self.0, size.x)?,
+            y: ToRange::checked(self.1, size.y)?,
+        })
+    }
+
+    fn unchecked(self, size: Size) -> Point<Range<usize>> {
+        Point {
+            x: ToRange::unchecked(self.0, size.x),
+            y: ToRange::unchecked(self.1, size.y),
+        }
+    }
+}
+
+pub(crate) mod flat {
+    use crate::{Major, Point};
+    use std::ops::Range;
+
+    pub trait Index0D {
+        fn index<M: Major>(self, size: M) -> usize;
+    }
+
+    impl Index0D for Point {
+        fn index<M: Major>(self, size: M) -> usize {
+            let point = M::from(self);
+
+            point.minor() * size.major() + point.major()
+        }
+    }
+
+    pub trait Index1D {
+        fn index<M: Major>(self, size: M) -> Range<usize>;
+    }
+
+    impl Index1D for (usize, Range<usize>) {
+        fn index<M: Major>(self, size: M) -> Range<usize> {
+            let (i, Range { start, end }) = self;
+            let point = M::new(start, i).into();
+            let start = point.index(size);
+
+            start..start + end
+        }
     }
 }
--- src/new/mod.rs
+++ src/new/mod.rs
@@ -1,16 +1,16 @@
 mod coord;
 mod flat;
 mod grid;
+mod index;
 mod iters;
-// mod index;
 mod major;
 mod range;
 
+pub use self::grid::*;
 pub use coord::*;
 pub use flat::*;
-pub use grid::*;
+pub use index::*;
 pub use iters::*;
-// pub use index::*;
 pub use major::*;
 pub use range::*;
 
@@ -33,144 +33,5 @@ impl<T: std::ops::Deref<Target = U>, U: WithSize> WithSize for T {
 impl<M: Major, T: std::ops::Deref<Target = U>, U: WithMSize<M>> WithMSize<M> for T {
     fn msize(&self) -> M {
         self.deref().msize()
-    }
-}
-
-pub use index::*;
-mod index {
-    use crate::*;
-    use std::ops::{Range, RangeBounds};
-
-    pub trait Index0D {
-        fn unchecked<S: Into<Size>>(self, size: S) -> Point;
-        fn checked<S: Into<Size>>(self, size: S) -> Option<Point>;
-    }
-
-    impl Index0D for Point {
-        fn unchecked<S: Into<Size>>(self, size: S) -> Point {
-            self
-        }
-
-        fn checked<S: Into<Size>>(self, size: S) -> Option<Point> {
-            if self < size.into() {
-                Some(self)
-            } else {
-                None
-            }
-        }
-    }
-
-    pub trait Index1D: Sized {
-        fn unchecked<M: Major>(self, size: M) -> (usize, Range<usize>);
-        fn checked<M: Major>(self, size: M) -> Option<(usize, Range<usize>)>;
-    }
-
-    impl Index1D for usize {
-        fn checked<M: Major>(self, size: M) -> Option<(usize, Range<usize>)> {
-            if self < size.minor() {
-                Some(self.unchecked(size))
-            } else {
-                None
-            }
-        }
-
-        fn unchecked<M: Major>(self, size: M) -> (usize, Range<usize>) {
-            (self, 0..size.major())
-        }
-    }
-
-    impl<T: RangeBounds<usize>> Index1D for (usize, T) {
-        fn checked<M: Major>(self, size: M) -> Option<(usize, Range<usize>)> {
-            let (i, range) = self;
-
-            if i < size.minor() {
-                Some((i, ToRange::checked(range, size.major())?))
-            } else {
-                None
-            }
-        }
-
-        fn unchecked<M: Major>(self, size: M) -> (usize, Range<usize>) {
-            (self.0, ToRange::unchecked(self.1, size.major()))
-        }
-    }
-
-    /// Indexing into a rectangle inside a grid, with optional cropping.
-    pub trait Index2D {
-        /// Returns the index **with** bounds checking.
-        fn checked(self, size: Size) -> Option<Point<Range<usize>>>;
-
-        /// Returns the index **without** bounds checking.
-        fn unchecked(self, size: Size) -> Point<Range<usize>>;
-    }
-
-    impl Index2D for () {
-        fn checked(self, size: Size) -> Option<Point<Range<usize>>> {
-            Some(self.unchecked(size))
-        }
-
-        fn unchecked(self, size: Size) -> Point<Range<usize>> {
-            Point {
-                x: 0..size.x,
-                y: 0..size.x,
-            }
-        }
-    }
-
-    impl<X: RangeBounds<usize>, Y: RangeBounds<usize>> Index2D for Point<X, Y> {
-        fn checked(self, size: Size) -> Option<Point<Range<usize>>> {
-            (self.x, self.y).checked(size)
-        }
-
-        fn unchecked(self, size: Size) -> Point<Range<usize>> {
-            (self.x, self.y).unchecked(size)
-        }
-    }
-
-    impl<X: RangeBounds<usize>, Y: RangeBounds<usize>> Index2D for (X, Y) {
-        fn checked(self, size: Size) -> Option<Point<Range<usize>>> {
-            Some(Point {
-                x: ToRange::checked(self.0, size.x)?,
-                y: ToRange::checked(self.1, size.y)?,
-            })
-        }
-
-        fn unchecked(self, size: Size) -> Point<Range<usize>> {
-            Point {
-                x: ToRange::unchecked(self.0, size.x),
-                y: ToRange::unchecked(self.1, size.y),
-            }
-        }
-    }
-
-    pub(crate) mod flat {
-        use crate::{Major, Point};
-        use std::ops::Range;
-
-        pub trait Index0D {
-            fn index<M: Major>(self, size: M) -> usize;
-        }
-
-        impl Index0D for Point {
-            fn index<M: Major>(self, size: M) -> usize {
-                let point = M::from(self);
-
-                point.minor() * size.major() + point.major()
-            }
-        }
-
-        pub trait Index1D {
-            fn index<M: Major>(self, size: M) -> Range<usize>;
-        }
-
-        impl Index1D for (usize, Range<usize>) {
-            fn index<M: Major>(self, size: M) -> Range<usize> {
-                let (i, Range { start, end }) = self;
-                let point = M::new(start, i).into();
-                let start = point.index(size);
-
-                start..start + end
-            }
-        }
     }
 }
