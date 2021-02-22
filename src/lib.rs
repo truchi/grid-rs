be0bb646de51f80a7b35ae7c40025f55ec2ffc15
Romain TRUCHI <romain.truchi.06@gmail.com>
2021-02-22 15:54:33 +0100

    ...

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,18 @@
 // #![warn(missing_docs)]
-// #![allow(unused)]
+
+pub mod grid1d;
+pub mod repeat;
+pub mod repeat_with;
+
+pub use grid1d::{ColGrid1D, Grid1D, RowGrid1D};
+pub use repeat::{repeat, Repeat};
+pub use repeat_with::RepeatWith;
 
 mod cloned;
 mod cropped;
 mod grid;
-pub mod grid1d;
 mod index;
 mod major;
-pub mod repeat;
-pub mod repeat_with;
 mod utils;
 mod with_msize;
 mod with_size;
@@ -16,11 +20,9 @@ mod with_size;
 pub use self::grid::*;
 pub use cloned::*;
 pub use cropped::*;
-pub use grid1d::{ColGrid1D, Grid1D, RowGrid1D};
 pub use index::*;
 pub use major::*;
-pub use repeat::{repeat, Repeat};
-pub use repeat_with::RepeatWith;
 pub use utils::*;
-pub(crate) use with_msize::*;
 pub use with_size::*;
+
+pub(crate) use with_msize::*;
